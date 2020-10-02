03c02b65ce746945ff247f1fa80981f800b63e49
Walter Ruoppa <walter.ruoppa@metropolia.fi>
2020-10-03 00:20:41 +0300

    toimii frontend kanssa

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+
+app.use(express.json())
+
+let postaukset = [
+  {
+    "id": 1,
+    "gif": "nuXIEASt1wqI",
+    "label": "goth",
+    "marker": "sad",
+    "pos": [
+      60.16985569999999,
+      24.938379
+    ]
+  }
+]
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+app.get('/api/postaukset', (request, response) => {
+  response.json(postaukset)
+})
+
+const generateId = () => {
+  const maxId = postaukset.length > 0
+    ? Math.max(...postaukset.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/postaukset', (request, response) => {
+  const body = request.body
+
+    const postaus = {
+    label: body.label,
+    marker: body.marker,
+    gif: body.gif,
+    pos: body.pos,
+    id: generateId(),
+  }
+
+  postaukset = postaukset.concat(postaus)
+
+  response.json(postaukset)
+})
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
