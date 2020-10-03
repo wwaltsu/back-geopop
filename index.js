045cf301bc951bdbe2f69ca0ae8c8a9c3fa11c61
Walter Ruoppa <walter.ruoppa@metropolia.fi>
2020-10-03 23:25:16 +0300

    tietokanta näkyy kartassa + pos fix

--- index.js
+++ index.js
@@ -1,59 +1,87 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const Post = require('./models/post')
 const cors = require('cors')
 app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
 
-
-app.use(express.json())
-
-let postaukset = [
-  {
-    "id": 1,
-    "gif": "nuXIEASt1wqI",
-    "label": "goth",
-    "marker": "sad",
-    "pos": [
-      60.16985569999999,
-      24.938379
-    ]
-  }
-]
-
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/api/posts', (request, response) => {
+  Post.find({}).then(posts => {
+    response.json(posts.map(post => post.toJSON()))
+  })
 })
 
-app.get('/api/postaukset', (request, response) => {
-  response.json(postaukset)
-})
-
-const generateId = () => {
-  const maxId = postaukset.length > 0
-    ? Math.max(...postaukset.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/postaukset', (request, response) => {
+app.post('/api/posts', (request, response, next) => {
   const body = request.body
+  console.log(body)
+  const post = new Post({
+    gif: body.gif,
+    label: body.label || false,
+    marker: body.marker,
+    pos: body.pos,
+  })
 
-    const postaus = {
+  post.save()
+    .then(savedPost => savedPost.toJSON())
+    .then(savedAndFormattedPost => {
+      response.json(savedAndFormattedPost)
+    }) 
+    .catch(error => next(error))
+})
+
+app.get('/api/posts/:id', (request, response, next) => {
+  Post.findById(request.params.id)
+    .then(post => {
+      if (post) {
+        response.json(post.toJSON())
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/posts/:id', (request, response, next) => {
+  const body = request.body
+  const post = {
+    gif: body.gif,
     label: body.label,
     marker: body.marker,
-    gif: body.gif,
     pos: body.pos,
-    id: generateId(),
   }
 
-  postaukset = postaukset.concat(postaus)
-
-  response.json(postaukset)
+  Post.findByIdAndUpdate(request.params.id, post, { new: true })
+    .then(updatedPost => {
+      response.json(updatedPost.toJSON())
+    })
+    .catch(error => next(error))
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
+app.use(unknownEndpoint)
 
-const PORT = 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT 
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ models/post.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+  const postSchema = new mongoose.Schema({
+    label: "String",
+    marker: "String",
+    gif: "String",
+    pos: [ "number", "number"],
+  })
+
+postSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Post', postSchema)
